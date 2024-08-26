70abbe992abc03ca5bd6bcb4de3a79beedb6e6df
Joaquim <oliver-junior@hotmail.com>
2024-08-26 20:37:13 -0300

    Create NewClass.cs

---
+++ CSharp/variables/NewClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharp.variables
+{
+    public class NewClass
+    {
+        Console.WriteLine("Hello!");
+    }
+}
