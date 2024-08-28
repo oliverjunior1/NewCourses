3c9dc33c35ddbac9d1b275a3d27652da3dbea2e8
Joaquim <oliver-junior@hotmail.com>
2024-08-28 20:50:46 -0300

    Create concatenation.java

---
+++ Java/concatenation.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class concatenation {
+    public static void main(String[] args) {
+        /* 
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println(firstName + " " + lastName);
+        */
+        String firstName = "John ";
+        String lastName = "Doe";
+        System.out.println(firstName.concat(lastName));
+        
+    }
+
+}
+
