18209dd350e25cc1e9fc25eef42e7dd22627e874
Joaquim <oliver-junior@hotmail.com>
2024-08-27 22:26:45 -0300

    Update variables.kt

--- kotlin/variables.kt
+++ kotlin/variables.kt
@@ -1,1 +1,11 @@
-println("Jesus is the light of the World")
+fun main() {
+    /*var name = "John"
+    val birthyear = 1975
+
+    println(name)
+    println(birthyear)
+    */
+    var name = String
+    name = "John"
+    println(name)
+}
