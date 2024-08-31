1eeaa5622ee3588fb602959abc66d37c7b5f7e95
Joaquim <oliver-junior@hotmail.com>
2024-08-30 22:43:48 -0300

    Create Teste.cs

---
+++ CSharp/variables/Teste.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharp.variables
+{
+    public class Teste
+    {
+        Console.WriteLine("Jesus is the light of the World!");
+        Console.ReadLine();
+    }
+}
