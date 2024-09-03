95b259a21a79882fe9456d21d4682af7b2ae5ed4
Joaquim <oliver-junior@hotmail.com>
2024-09-03 19:21:41 -0300

    Update Treinamento.java

--- Java/Treinamento.java
+++ Java/Treinamento.java
@@ -2,7 +2,19 @@ package Java;
 
 public class Treinamento {
     public static void main(String[] args) {
+        /*
+        //a - Math 
         double x = Math.max(5,10);
         System.out.println(x);
+        */
+        //b - if else
+        int x = 20;
+        if (x > 18) {
+            System.out.println("You can drive, go on!");
+        }
+        else {
+            System.out.println("You can't drive, left your car!");
+        }
+
     }
 }
