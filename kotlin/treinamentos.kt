5312537327515bff719069ecd84739ccd135258e
Joaquim <oliver-junior@hotmail.com>
2024-09-03 20:33:27 -0300

    Update treinamentos.kt

--- kotlin/treinamentos.kt
+++ kotlin/treinamentos.kt
@@ -1,9 +1,20 @@
+
+
 fun main() {
+    /* 
     //while
     var i = 0
     while (i < 5) {
         println(i)
         i++
     }
+//--------------------------------------
+*/
+// Arrays
+val cars = arrayOf("Volvo", "BMW", "Ford", "Mazda")
 
+for(car in cars) {
+    println(car)
 }
+}
+
