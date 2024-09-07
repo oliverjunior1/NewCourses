12652339979b3e1d7aabe211fdfcc4ed4c47e089
Joaquim <oliver-junior@hotmail.com>
2024-09-07 17:13:37 -0300

    Update Treinamento.java

--- Java/Treinamento.java
+++ Java/Treinamento.java
@@ -16,11 +16,40 @@ public class Treinamento {
         else {
             System.out.println("You can't drive, left your car!");
         }
-        */
+        
         //--------------------c - short hand if else --------------------
         int time = 20;
         String result = (time < 18)? "Good day." : "Good evening.";
         System.out.println(result);
-
+        */
+        //---------------------Switch-------------------------------------
+        int day = 4;
+        switch(day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("not a accepted number, type from 1 to 7.");
+                break;
+        
+        }
     }
 }
