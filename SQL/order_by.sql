64d01b5e230b4634081f1d198791b9f070ce4d01
oliverjunior1 <oliver-junior@hotmail.com>
2024-09-24 22:57:11 -0300

    Create order_by.sql

---
+++ SQL/order_by.sql
@@ -0,0 +1,6 @@
+/*
+SELECT * FROM customer
+ORDER BY first_name ASC; --In alphabetically crescent order.
+*/
+SELECT store_id, first_name, last_name FROM customer
+ORDER BY store_id, first_name ASC;
