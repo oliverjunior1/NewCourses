a9a0b79333ff4f3458050f2af8d93fcc34d7e447
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-01 20:03:51 -0300

    kotlin update

---
+++ kotlin/Catalin Stefan/strings.kt
@@ -0,0 +1,4 @@
+fun main() {
+    println("The anwer is \"yes\"so you can proceed")
+    println("I like to put a \\ in my string")
+}
---
+++ kotlin/w3 schools/variables.kt
@@ -0,0 +1,11 @@
+fun main() {
+    /*var name = "John"
+    val birthyear = 1975
+
+    println(name)
+    println(birthyear)
+    */
+    var name = String
+    name = "John"
+    println(name)
+}
