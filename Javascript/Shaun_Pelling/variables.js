f92d281e843733f2241023600d65681d14cde519
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-01 22:24:30 -0300

    Update variables.js

--- Javascript/Shaun_Pelling/variables.js
+++ Javascript/Shaun_Pelling/variables.js
@@ -1,11 +1,13 @@
 let age = 25
 let year = 2019
 
+// log things to console
 console.log(age, year)
 
 age = 30
 console.log(age)
 
+// It's Coudn't change
 const points = 100
 console.log(points)
 
