8e001e951ca4e62f57b7400a9c1c95b1a2948b0e
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-01 22:24:36 -0300

    Create challenge_ORDER_BY.sql

---
+++ SQL/challenge_ORDER_BY.sql
@@ -0,0 +1,11 @@
+/*
+SELECT customer_id FROM payment
+ORDER BY payment_date ASC
+LIMIT 10;
+
+SELECT title, length FROM film
+ORDER BY length ASC
+LIMIT 5;
+*/
+SELECT COUNT(title) FROM film
+WHERE length <= 50;
