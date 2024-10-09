b2d10251ba5c57ea91c81f2a107b6409bca7a027
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-08 23:08:25 -0300

    Create variables.kt

---
+++ kotlin/Catalin Stefan/variables.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // creating (assigning) a variable
+    var tvShowsWatched = "many"
+    var dogs = 3
+
+    // changing a variable
+    var age = 25
+    println(age)
+    age = 26
+    println(age)
+    var newAge = age + 1
+
+    println(newAge)
+
+}
