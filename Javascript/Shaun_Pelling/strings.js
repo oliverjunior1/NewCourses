4b646fd1d369b385f11d44e559c97bc658a960cd
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-08 22:47:01 -0300

    Create strings.js

---
+++ Javascript/Shaun_Pelling/strings.js
@@ -0,0 +1,25 @@
+// strings
+console.log('Hello, world!')
+
+let email = 'joaquim.o.junior@gmail.com'
+console.log(email)
+
+// string concatenation
+let firstName = "Joaquim"
+let lastName = "Rodrigues"
+
+let fullName = firstName + " " + lastName
+console.log(fullName)
+
+//getting characters
+console.log(fullName[0])
+
+// string length
+console.log(fullName.length)
+
+// string methods
+console.log(fullName.toUpperCase())
+console.log(fullName.toLowerCase())
+console.log(fullName.indexOf('o'))
+console.log(fullName.indexOf())
+console.log(fullName.length)
