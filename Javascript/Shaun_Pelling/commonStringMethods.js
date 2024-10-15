b4880ec7e5069c2423b97d4dcc87d86509e10246
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-15 19:37:16 -0300

    Create commonStringMethods.js

---
+++ Javascript/Shaun_Pelling/commonStringMethods.js
@@ -0,0 +1,9 @@
+let email = 'Joaquim@hotmail.com'
+
+//let result = email.lastIndexOf('m')
+//let result = email.slice(0,7)
+//let result = email.substring(0,10)
+//let result = email.replace('m', 'w')
+let result = email.replace('o','w' )
+
+console.log(result)
