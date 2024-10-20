be624258eed918299b92e7c9e6771dc5fc33920c
oliverjunior1 <oliver-junior@hotmail.com>
2024-10-20 12:27:04 -0300

    Update ConsoleApp1.AssemblyInfo.cs

--- CSharp/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ CSharp/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18ed5671304b70f626e90148268f50065a9195b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba8de61a4399fc7252be02578c07cf6948df3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
